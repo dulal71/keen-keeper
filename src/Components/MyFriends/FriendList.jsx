ad88da55d8ceced387ef73e07d0a51bf4a27928f
dulal71 <ahmeddulal4211@gmail.com>
2026-04-17 17:36:57 +0600

    add style in timeline list titel

--- src/Components/MyFriends/FriendList.jsx
+++ src/Components/MyFriends/FriendList.jsx
@@ -7,7 +7,7 @@ const FriendList = ({friends}) => {
     return (
         <div>
           <div className='pt-5'>
-<h2 className='font-bold text-3xl'>Your Friends</h2>
+<h2 className='font-bold text-3xl bg-linear-to-r from-[#244d3f] via-[#278163] to-[#34ac84] bg-clip-text text-transparent'>Your Friends</h2>
 <div className='grid md:grid-cols-3 lg:grid-cols-4 gap-8 mt-10'>
     {
      friends.map(friend=><Friend key={friend.id} friend={friend}></Friend>)
