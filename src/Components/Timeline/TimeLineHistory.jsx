aeed289c8186736a1f9aab7bbd4a70a54dd30429
dulal71 <ahmeddulal4211@gmail.com>
2026-04-17 11:18:36 +0600

    add stats page and a piechart

--- src/Components/Timeline/TimeLineHistory.jsx
+++ src/Components/Timeline/TimeLineHistory.jsx
@@ -19,9 +19,9 @@ const TimeLineHistory = ({data}) => {
         <div className='max-w-3xl mx-auto'>
             <h1 className='font-bold text-3xl md:text-4xl lg:text-5xl bg-linear-to-r from-[#244d3f] via-[#278163] to-[#34ac84] bg-clip-text text-transparent mb-2'>Timeline </h1>
             <div>
-             <div className="dropdown dropdown-center">
+             <div className="dropdown dropdown-center ml-5">
   <div tabIndex={0} role="button" className="btn m-1">Filter timeline </div>
-  <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+  <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm ">
     <li onClick={()=>setSort('all')}><a>All</a></li>
     <li onClick={()=>setSort('text')}><a>Text</a></li>
     <li onClick={()=>setSort('call')}><a>Call</a></li>
